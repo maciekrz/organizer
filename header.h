a1e587be9174553d1b18494e62c38495b713b4b0
maciekrz <macikrz683@student.polsl.pl>
2021-06-17 10:31:27 +0200

    split into header and main

--- header.h
+++ header.h
@@ -1,11 +1,9 @@
-/*
+#pragma once
 #include <map>
 #include <string>
 #include <iostream>
 #include <fstream>
-*/
-#include"header.h"
-/*
+
 class Organizer
 {
 
@@ -289,26 +287,6 @@ void interactiveMode(Organizer& list)
     }
     return;
 }
-*/
 
-int main()
-{
-    Organizer home("HOME"), work("WORK");
-    ShoppingList SL("SHOPPING LIST");
-    std::cout << "\nType in the name of a list to see its content or \"end\" to exit the program\n";
-    std::string listName = "";
-    while (1)
-    {
-        std::cout << "\nChoose the list:\n[ home (" << home.countTasks() << ") ][ work (" << work.countTasks() << ") ][ shopping (" << SL.countTasks() << ") ][ end ]: ";
-        std::cin >> listName;
-        if ("home" == listName)             interactiveMode(home);
-        else if ("work" == listName)        interactiveMode(work);
-        else if ("shopping" == listName)    interactiveMode(SL);
-        else if ("end" == listName)         break;
-        else                                std::cout << "Wrong name!";
-    }
-    
-    std::cout << "\n";
-    return 0;
-}
+
 
